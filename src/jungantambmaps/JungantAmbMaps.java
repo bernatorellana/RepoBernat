d4336d1bd1c816a56de62fb9c45ee1d905688ee5
Bernat <bernat.orellana@gmail.com>
2020-10-04 21:07:32 +0200

    canvi B

--- src/jungantambmaps/JungantAmbMaps.java
+++ src/jungantambmaps/JungantAmbMaps.java
@@ -104,7 +104,7 @@ public class JungantAmbMaps {
         }
 
         //
-        System.out.println("He fet un canvi DES DEL PRINCIPAL");
+        System.out.println("He fet un canvi DES DEL PRINCIPAL AAAAAAAA");
         
         // Canvi a la còpia
         System.out.println("Sóc còpia");
